c25638b108d35c41e32a7c58875c5c498f342734
Master Kimmy <hakeemkolawole62@gmail.com>
2024-07-04 11:30:57 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 import Nav from "./component/Nav"
 import Section from './component/Section'
 import Profile from './component/profile'
+import About from './component/About'
 import Footer from "./component/footer"
+import Privacy from "./component/Privacy"
 function App() {
 
 
   return (
     <>
-    <Nav/>
-     <Section/>
-     <Profile/>
-     <Footer/>
+    <Nav /> 
+   {/* <Profile/> */}
+    {/* <Section/>  */}
+      <About />
+      <Privacy />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
